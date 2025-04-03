50544b2341e3ffef0b38b9174e101c15ae0d6aa0
LeBelin <andy.belin64@gmail.com>
2025-04-03 13:27:45 +0200

    add login api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ApiController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//Route::post('register', [ApiController::class, 'register']);
+Route::post('login', [ApiController::class, 'login']);
+
+//Route::middleware(['auth:sanctum' => ["auth:sanctum"] ], function() {
+//    Route::get('rendezvous', [ApiController::class, 'rendezvous']);
+//});
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
